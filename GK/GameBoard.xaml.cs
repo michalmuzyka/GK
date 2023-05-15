cc6a56f25ddf69d23d6bde7ac52094e9eb1eb97b
Michał Muzyka <michal.muzyka@o2.pl>
2023-05-15 23:27:22 +0200

    kilka klas

---
+++ GK/GameBoard.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GK
+{
+    /// <summary>
+    /// Logika interakcji dla klasy GameBoard.xaml
+    /// </summary>
+    public partial class GameBoard : Page
+    {
+        public GameBoard()
+        {
+            InitializeComponent();
+        }
+    }
+}
